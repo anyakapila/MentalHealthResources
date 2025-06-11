492ea5d861853ab066bd0564ac47d1f163f3e901
Anya <anyakapila@gmail.com>
2025-06-11 13:35:30 -0700

    fixed wrapping

--- src/views/Support.vue
+++ src/views/Support.vue
@@ -3,7 +3,7 @@
     <div class="header">
       <img src="@/assets/newlogo.png" alt="Logo" class="logo"/>
       <div class="header-text">
-        <h1>Support Providers</h1>
+        <h1>Support</h1>
         <h2>Find the therapist, psychiatrist, or other support provider for you.</h2>
       </div>
     </div>
@@ -12,7 +12,7 @@
     <ul class="nav-links">
       <router-link to="/">Home</router-link> |
       <router-link to="/why">Why?</router-link> |
-      <router-link to="/support">Support Providers</router-link> |
+      <router-link to="/support">Support</router-link> |
       <router-link to="/hotlines">Hotlines</router-link> |
       <router-link to="/articles">Articles</router-link>
     </ul>
--- src/views/Why.vue
+++ src/views/Why.vue
@@ -12,7 +12,7 @@
       <ul class="nav-links">
         <router-link to="/">Home</router-link> |
         <router-link to="/why">Why?</router-link> |
-        <router-link to="/support">Support Providers</router-link> |
+        <router-link to="/support">Support</router-link> |
         <router-link to="/hotlines">Hotlines</router-link> |
         <router-link to="/articles">Articles</router-link>
       </ul>
