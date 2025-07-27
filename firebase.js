2bea4d849ac9ed1519f4624fb3c4dcc1be2da177
Anya <anyakapila@gmail.com>
2025-07-26 21:32:19 -0700

    initializing firebase

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCsIkWtyFXPXh7lFbmwEyJNXokDtBlcfe4",
+  authDomain: "anya-mentalhealthresources.firebaseapp.com",
+  projectId: "anya-mentalhealthresources",
+  storageBucket: "anya-mentalhealthresources.firebasestorage.app",
+  messagingSenderId: "790233159595",
+  appId: "1:790233159595:web:24424f7ae94d58e25564ee"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
