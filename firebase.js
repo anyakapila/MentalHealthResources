c6013b907b9802eb4b3105646e03b083be18ffb7
Anya <anyakapila@gmail.com>
2025-07-26 21:52:59 -0700

    initializing database

--- firebase.js
+++ firebase.js
@@ -1,5 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getDatabase, ref, set } from "firebase/database";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -15,3 +16,13 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+function writeUserData(userId, name) {
+  const database = getDatabase();
+  const reference = ref(database, 'users/' + userId);
+  set(reference, {
+    username: name
+  }); 
+}
+
+writeUserData('1', 'Anya'); // Example usage, replace with actual user data
